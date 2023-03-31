6ef29df90ae9a5373cc762999169c820e314f296
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-03-31 17:59:04 +0200

    fixes style for song detail

---
+++ resources/views/partials/_song_detail_card.blade.php
@@ -0,0 +1,40 @@
+<div class="my-card text-center">
+    <figure>
+        <img src="{{$song->poster}}" alt="{{$song->title}} poster">
+    </figure>
+
+    <ul class="p-0 my-3">
+        <li>
+            <span class="d-block fs-5 fw-semibold">
+                Album:
+            </span>
+            <span class="fw-semibold text-muted">
+                {{$song->album}}
+            </span>
+        </li>
+        <li>
+            <span class="d-block fs-5 fw-semibold">
+                Autore:
+            </span>
+            <span class="fw-semibold text-muted">
+                {{$song->author}}
+            </span>
+        </li>
+        <li>
+            <span class="d-block fs-5 fw-semibold">
+                Editore:
+            </span>
+            <span class="fw-semibold text-muted">
+                {{$song->editor}}
+            </span>
+        </li>
+        <li>
+            <span class="d-block fs-5 fw-semibold">
+                Durata:
+            </span>
+            <span class="fw-semibold text-muted">
+                {{$song->length}} m
+            </span>
+        </li>
+    </ul>
+</div>
--- resources/views/songs/show.blade.php
+++ resources/views/songs/show.blade.php
@@ -7,18 +7,7 @@
 @section('main_content',)
 <div class="row justify-content-center my-5">
     <div class="col-4">
-        <div class="my-card text-center">
-            <figure>
-                <img src="{{$song->poster}}" alt="{{$song->title}} poster">
-            </figure>
-
-            <ul class="p-0 my-3">
-                <li>{{$song->album}}</li>
-                <li>{{$song->author}}</li>
-                <li>{{$song->editor}}</li>
-                <li>{{$song->length}} m</li>
-            </ul>
-        </div>
+        @include('partials._song_detail_card')
     </div>
 </div>
 
