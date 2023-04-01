0908415eb24eb03b9bd4747a18c62c9552e3305a
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-04-01 11:49:57 +0200

    use Bootstrap for pagination

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Faccio in modo di poter utilizzare Bootstrap per la paginazione
+        Paginator::useBootstrap();
     }
 }
